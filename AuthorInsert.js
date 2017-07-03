c99988d56a12fd100c6b2d3974a292f2fe7536b8
Benknightdark <moon791017@gmail.com>
2017-07-03 22:36:30 +0800

    dd

--- AuthorInsert.js
+++ AuthorInsert.js
@@ -8,33 +8,55 @@ r.connect({
     if (err) throw err;
     connection = conn;
     r.table('authors').insert([{
-            name: "William Adama",
-            tv_show: "test",
-            posts: [{
-                    title: "aaa",
-                    content: "ccc"
-                },
-                {
-                    title: "bbb",
-                    content: "ddd"
-                },
-                {
-                    title: "The new Earth",
-                    content: "The discoveries of the past few days..."
-                }
-            ]
-        }      
-    ]).run(connection, function (err, result) {
+        name: "William Adama",
+        tv_show: "test",
+        posts: [{
+                title: "aaa",
+                content: "ccc"
+            },
+            {
+                title: "bbb",
+                content: "ddd"
+            },
+            {
+                title: "The new Earth",
+                content: "The discoveries of the past few days..."
+            }
+        ]
+    }]).run(connection, function (err, result) {
         if (err) throw err;
         console.log(JSON.stringify(result, null, 2));
     })
 
 
+r.table('authors').update({type: "fictional"}).
+    run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    });
+
+r.table('authors').
+    filter(r.row("name").eq("test")).
+    update({rank: "Admiral"}).
+    run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    });
 
 
-   
-
-   
-
-
+r.table('authors').filter(r.row("name").eq("test")).
+    update({posts: r.row("posts").append({
+        title: "Shakespeare",
+        content: "What a piece of work is man..."})
+    }).run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    });
+r.table('authors').
+    filter(r.row('posts').count().lt(3)).
+    delete().
+    run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    });
 })
